9dcb12ede794e09fcac7f435ae91faef4a4d5477
Saurabh Prabhuzantye <saurabh@Saurabhs-Laptop.local>
2025-03-15 02:48:15 +0530

    Fix __init__.py formatting

--- icp_analyzer/src/__init__.py
+++ icp_analyzer/src/__init__.py
@@ -1,5 +1,6 @@
 from .analyzer import ICPAnalyzer
 from .scraper import WebScraper
 from .processor import ContentProcessor
+from .example import analyze_and_print, main
 
-__all__ = ['ICPAnalyzer', 'WebScraper', 'ContentProcessor'] 
+__all__ = ['ICPAnalyzer', 'WebScraper', 'ContentProcessor', 'analyze_and_print', 'main']
